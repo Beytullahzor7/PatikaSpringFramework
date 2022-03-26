4ba4d9685a78d210da16a81ce976466b547a47e5
Beytullahzor7 <beytullahzor7@gmail.com>
2022-03-26 17:53:06 +0300

    Ioc(-) Dependency Injection(-)

---
+++ src/main/java/com/patika/iocsiz_disiz/Calisan.java
@@ -0,0 +1,18 @@
+package com.patika.iocsiz_disiz;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+
+@Named(value = "cdisizCalisan")
+@ApplicationScoped
+public class Calisan {
+    private Patron patron;
+
+    public Calisan() {
+        patron = new Patron();
+    }
+
+    public String getData(String data){
+        return patron.version(data);
+    }
+}
---
+++ src/main/java/com/patika/iocsiz_disiz/Patron.java
@@ -0,0 +1,8 @@
+package com.patika.iocsiz_disiz;
+
+public class Patron {
+
+    public String version(String data){
+        return "sürüm " + data;
+    }
+}
